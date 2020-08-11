a43e9c381092196d689df3f8ce579f89d1311d3e
Marcelo Raimo <raimomarcelo@gmail.com>
2020-08-11 18:42:31 -0300

    mobile update
    update

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F0F7',
+    },
+
+})
+
+export default styles;
